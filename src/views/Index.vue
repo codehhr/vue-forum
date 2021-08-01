24960f8e1b60203f9dd4a5352165f98372f55a6c
codehhr <1871973389@qq.com>
2021-08-01 23:50:54 +0800

    create Index and import PageHeader

---
+++ src/views/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <page-header></page-header>
+  </div>
+</template>
+
+<script>
+import PageHeader from "../components/PageHeader";
+export default {
+  name: "Index",
+  components: {
+    PageHeader,
+  },
+};
+</script>
+
+<style scoped></style>

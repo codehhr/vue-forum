a96e49804bdd5afd29ed599d2ac141b8df216a79
codehhr <1871973389@qq.com>
2021-08-02 11:00:10 +0800

    reste styles

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="less">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, PingFang SC,
+    Hiragino Sans GB, Microsoft YaHei, Helvetica Neue, Helvetica, Arial,
+    sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol;
+}
+
+a {
+  display: inline-block;
+  text-decoration: none;
+  color: #42b983;
+}
+
+ul {
+  list-style: none !important;
+}
+
+img {
+  display: block;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;

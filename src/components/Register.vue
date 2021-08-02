714a7892f0f51194febced5a06f8629c58df623d
codehhr <1871973389@qq.com>
2021-08-02 21:10:45 +0800

    完成注册功能

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,25 +1,23 @@
 <template>
-  <a-form :form="form" @submit="handleSubmit">
-    <a-form-item v-bind="formItemLayout" label="E-mail">
+  <a-form class="register-form" :form="form" @submit="handleRegisterSubmit">
+    <a-form-item v-bind="formItemLayout">
       <a-input
         v-decorator="[
-          'email',
+          'loginName',
           {
             rules: [
               {
-                type: 'email',
-                message: 'The input is not valid E-mail!',
-              },
-              {
                 required: true,
-                message: 'Please input your E-mail!',
+                message: 'Please input your username!',
               },
             ],
           },
         ]"
+        placeholder="请输入用户名"
+        autocomplete
       />
     </a-form-item>
-    <a-form-item v-bind="formItemLayout" label="Password" has-feedback>
+    <a-form-item v-bind="formItemLayout" has-feedback>
       <a-input
         v-decorator="[
           'password',
@@ -35,10 +33,12 @@
             ],
           },
         ]"
+        autocomplete
+        placeholder="请输入密码"
         type="password"
       />
     </a-form-item>
-    <a-form-item v-bind="formItemLayout" label="Confirm Password" has-feedback>
+    <a-form-item v-bind="formItemLayout" has-feedback>
       <a-input
         v-decorator="[
           'confirm',
@@ -54,20 +54,16 @@
             ],
           },
         ]"
+        autocomplete
+        placeholder="请确认您的密码"
         type="password"
         @blur="handleConfirmBlur"
       />
     </a-form-item>
     <a-form-item v-bind="formItemLayout">
-      <span slot="label">
-        Nickname&nbsp;
-        <a-tooltip title="What do you want others to call you?">
-          <a-icon type="question-circle-o" />
-        </a-tooltip>
-      </span>
       <a-input
         v-decorator="[
-          'nickname',
+          'userName',
           {
             rules: [
               {
@@ -78,13 +74,16 @@
             ],
           },
         ]"
+        autocomplete
+        placeholder="请输入昵称"
       />
     </a-form-item>
-
-    <a-form-item v-bind="formItemLayout" label="Phone Number">
+    <a-form-item v-bind="formItemLayout">
       <a-input
+        autocomplete
+        placeholder="请输入手机号"
         v-decorator="[
-          'phone',
+          'phonenumber',
           {
             rules: [
               { required: true, message: 'Please input your phone number!' },
@@ -107,54 +106,61 @@
         </a-select>
       </a-input>
     </a-form-item>
-
-    <a-form-item
-      v-bind="formItemLayout"
-      label="Captcha"
-      extra="We must make sure that your are a human."
-    >
-      <a-row :gutter="8">
-        <a-col :span="12">
+    <a-form-item v-bind="formItemLayout">
+      <a-row :gutter="24">
+        <a-col :span="16">
           <a-input
             v-decorator="[
-              'captcha',
+              'code',
               {
                 rules: [
                   {
-                    required: true,
-                    message: 'Please input the captcha you got!',
+                    message: '请输入验证码!',
                   },
                 ],
               },
             ]"
+            autocomplete
+            placeholder="请输入验证码"
+            title="请输入验证码"
           />
         </a-col>
-        <a-col :span="12">
-          <a-button>Get captcha</a-button>
+        <a-col :span="8">
+          <a-button
+            :disabled="registerCodeBtnDisabled"
+            class="get-code"
+            @click="getRegisterCode"
+          >
+            <span v-show="!hadRequestRegisterCode">获取验证码</span>
+            <span v-show="hadRequestRegisterCode">{{ registerSecond }}</span>
+          </a-button>
         </a-col>
       </a-row>
     </a-form-item>
     <a-form-item v-bind="tailFormItemLayout">
-      <a-checkbox v-decorator="['agreement', { valuePropName: 'checked' }]">
-        I have read the
-        <a href="">
-          agreement
-        </a>
-      </a-checkbox>
-    </a-form-item>
-    <a-form-item v-bind="tailFormItemLayout">
-      <a-button type="primary" html-type="submit">
-        Register
+      <a-button type="primary" html-type="submit" class="register-form-btn">
+        注册
       </a-button>
     </a-form-item>
   </a-form>
 </template>
 
 <script>
+import { register, getCode } from "../api/api";
+
 export default {
   name: "Register",
   data() {
     return {
+      // 获取注册验证码定时器
+      registerCodeInterval: null,
+      // 获取验证码按钮禁用
+      registerCodeBtnDisabled: false,
+      // 已经请求验证码
+      hadRequestRegisterCode: false,
+      // 请求验证码倒计时
+      registerSecond: 1 * 60,
+      // 注册验证规则
       confirmDirty: false,
       autoCompleteResult: [],
       formItemLayout: {
@@ -182,26 +188,44 @@ export default {
     };
   },
   methods: {
-    handleSubmit(e) {
+    // 注册 start
+    handleRegisterSubmit(e) {
       e.preventDefault();
       this.form.validateFieldsAndScroll((err, values) => {
         if (!err) {
-          console.log("Received values of form: ", values);
+          register(
+            values.loginName,
+            values.password,
+            values.userName,
+            values.phonenumber,
+            values.code
+          ).then((res) => {
+            console.log(res);
+            if (res.code === 0) {
+              this.$router.push({ name: "login" });
+              this.$message.success("注册成功,现在可以登录啦~");
+            } else {
+              this.$message.success(res.msg);
+            }
+          });
         }
       });
     },
+
     handleConfirmBlur(e) {
       const value = e.target.value;
       this.confirmDirty = this.confirmDirty || !!value;
     },
+    // 验证密码 start
     compareToFirstPassword(rule, value, callback) {
       const form = this.form;
       if (value && value !== form.getFieldValue("password")) {
-        callback("Two passwords that you enter is inconsistent!");
+        callback("两次密码输入不一致!");
       } else {
         callback();
       }
     },
+
     validateToNextPassword(rule, value, callback) {
       const form = this.form;
       if (value && this.confirmDirty) {
@@ -209,9 +233,63 @@ export default {
       }
       callback();
     },
+    // 验证密码 end
+
+    // 获取注册验证码
+    getRegisterCode() {
+      let that = this;
+      // 获取验证码倒计时
+      function countDown() {
+        // 按钮禁用
+        that.registerCodeBtnDisabled = true;
+
+        // 按钮倒计时 start
+        that.hadRequestRegisterCode = true;
+        if (!that.registerCodeInterval) {
+          that.registerCodeInterval = setInterval(() => {
+            if (that.registerSecond <= 0) {
+              clearInterval(that.registerCodeInterval);
+              that.registerCodeBtnDisabled = false;
+              that.hadRequestRegisterCode = false;
+            } else {
+              that.registerSecond--;
+            }
+          }, 1000);
+        }
+        // 按钮倒计时 end
+      }
+
+      // 获取验证码
+      this.form.validateFieldsAndScroll((err, values) => {
+        if (!err) {
+          getCode(values.phonenumber).then((res) => {
+            if (res.code === 0) {
+              countDown();
+            } else {
+              this.$message.warning(res.msg);
+            }
+          });
+        }
+      });
+    },
+
+    // 注册 end
   },
   beforeCreate() {
     this.form = this.$form.createForm(this, { name: "register" });
   },
 };
 </script>
+
+<style scoped lang="less">
+.register-form {
+  padding: 20px;
+  .get-code {
+    width: 100%;
+  }
+  .register-form-btn {
+    width: 100%;
+    width: 100%;
+  }
+}
+</style>

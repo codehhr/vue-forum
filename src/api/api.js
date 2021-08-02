891c9c0e910e4e68181763db877455b65aae916e
codehhr <1871973389@qq.com>
2021-08-02 21:04:12 +0800

    登录 api

--- src/api/api.js
+++ src/api/api.js
@@ -6,3 +6,12 @@ import axios from "./http";
 export function getTopicsList() {
   return axios.post("/forum/api/bbs/bbsCategory/open/list");
 }
+
+// 登录
+export function login(username, password) {
+  let formData = new FormData();
+  formData.append("username", username);
+  formData.append("rememberMe", true);
+  formData.append("password", password);
+  return axios.post("/forum/api/login", formData);
+}

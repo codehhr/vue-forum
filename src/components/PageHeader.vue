04b5ae19cc97b59ec8b108461ce9bdaf082aea58
codehhr <1871973389@qq.com>
2021-08-02 11:09:48 +0800

    导入子组件

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-    <h1>PageHeader</h1>
+    <!-- 左侧导航 -->
+    <header-nav></header-nav>
+    <!-- 切换论题 -->
+    <switch-topics></switch-topics>
+    <!-- 发表新帖 -->
+    <header-post></header-post>
   </div>
 </template>
 
 <script>
+import HeaderNav from "./HeaderNav";
+import HeaderPost from "./HeaderPost";
+import SwitchTopics from "./SwitchTopics";
+
 export default {
   name: "PageHeader",
+  components: {
+    HeaderNav,
+    HeaderPost,
+    SwitchTopics,
+  },
 };
 </script>
 

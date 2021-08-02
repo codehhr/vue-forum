082e41c16f41c41860861cfcb9c5458cc343e72f
codehhr <1871973389@qq.com>
2021-08-02 17:00:05 +0800

    add login state

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,17 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    // 分类
     category: null,
+    // 登录状态
+    alreadyLogin: false,
   },
   mutations: {
     setCategory(state, payload) {
       state.category = payload;
+    },
+    setLoginStatus(state, payload) {
+      state.alreadyLogin = payload;
     },
   },
   actions: {},

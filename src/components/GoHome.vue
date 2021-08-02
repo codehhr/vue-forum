561f52f54a53f5b7240e35cb45d508e3f7991b3d
codehhr <1871973389@qq.com>
2021-08-02 23:46:30 +0800

    create GoHome component

---
+++ src/components/GoHome.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="go-home">
+    <router-link to="/">
+      <van-button class="go-home-btn" icon="wap-home-o" />
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GoHome",
+};
+</script>
+
+<style lang="less" scoped>
+.go-home {
+  margin-bottom: 10px;
+  padding: 0 20px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  height: 46px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.4);
+  .go-home-btn {
+    padding: 0;
+  }
+}
+</style>

e137191ea1ba646d2f36c0dfb3f12774296d0b76
codehhr <1871973389@qq.com>
2021-08-02 19:36:40 +0800

    create Agreement

---
+++ src/components/Agreement.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="agreement">
+    <h1>协议就是[没有协议]</h1>
+    <img
+      width="40"
+      src="https://codehhr.coding.net/p/codehhr/d/images/git/raw/master/emoji/ziya.jpg"
+      alt=""
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Agreement",
+};
+</script>
+
+<style scoped lang="less">
+.agreement {
+  height: 60vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.2rem;
+  h1 {
+    margin: 0;
+  }
+}
+</style>

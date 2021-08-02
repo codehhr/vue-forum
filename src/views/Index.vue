a825bfdc6d1b4dd1b973d977bbc1d34f1d3507fa
codehhr <1871973389@qq.com>
2021-08-02 11:00:41 +0800

    create Index

---
+++ src/views/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <page-header></page-header>
+  </div>
+</template>
+
+<script>
+import PageHeader from "../components/PageHeader";
+
+export default {
+  name: "Index",
+  components: {
+    PageHeader,
+  },
+};
+</script>
+
+<style scoped></style>

22b46ff245d550d7854a640f8171ed7a97c69bfd
codehhr <1871973389@qq.com>
2021-08-03 00:15:20 +0800

    create Search

---
+++ src/components/Search.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="search-keyword">
+    <a-input-search
+      placeholder="搜索关键字"
+      style="width: 200px"
+      class="search-keyword"
+      @search="onSearch"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+};
+</script>
+
+<style scoped></style>

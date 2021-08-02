f8ce3de58f193b60f7b651f2579f236ac8c548a0
codehhr <1871973389@qq.com>
2021-08-02 19:37:17 +0800

    set router for User and Agreement

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import VueRouter from "vue-router";
 import Index from "../views/Index";
 import Login from "../components/Login";
 import Register from "../components/Register";
+import UserCenter from "../views/UserCenter";
+import Agreement from "../components/Agreement";
 
 Vue.use(VueRouter);
 
@@ -21,6 +23,16 @@ const routes = [
     path: "/register",
     name: "register",
     component: Register,
+  },
+  {
+    path: "/user",
+    name: "user",
+    component: UserCenter,
+  },
+  {
+    path: "/agreement",
+    name: "agreement",
+    component: Agreement,
   },
 ];
 

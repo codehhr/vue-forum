7fd09ea65adeb3a80cbae66467ca7dc75d297231
codehhr <1871973389@qq.com>
2021-08-03 16:54:03 +0800

    _

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// Vant
 import Vant from "vant";
 import "vant/lib/index.css";
+// Ant-Design
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/antd.less";
 

1bc3797b1a139270c690d41bc68a3f30007a1c51
codehhr <1871973389@qq.com>
2021-08-03 10:57:14 +0800

    完成个人中心入口

---
+++ src/components/AboutEntrance.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-show="alreadyLogin" class="about-me-entrance">
+    <a-icon class="bell-icon" type="sound" />
+    <van-button class="about-me-btn">关于我的</van-button>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "AboutEntrance",
+  computed: {
+    ...mapState({
+      alreadyLogin: "alreadyLogin",
+    }),
+  },
+};
+</script>
+
+<style scoped lang="less">
+.about-me-entrance {
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  .bell-icon {
+    width: 24px;
+    font-size: 1rem;
+  }
+  .about-me-btn {
+    margin: 0 10px;
+  }
+}
+</style>

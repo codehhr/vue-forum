382aa9a70d4781875bebea1b44ea2e1a495b3f2f
codehhr <1871973389@qq.com>
2021-08-03 19:14:35 +0800

    create PostDetail component

--- src/components/PostDetail.vue
+++ src/components/PostDetail.vue
@@ -1,11 +1,28 @@
 <template>
-  <div></div>
+  <div class="post-detail">
+    <go-home></go-home>
+    <div class="post-detail-header"></div>
+    <div class="post-detail-body"></div>
+    <div class="post-detail-comments"></div>
+  </div>
 </template>
 
 <script>
+import GoHome from "../components/GoHome";
 export default {
   name: "PostDetail",
+  methods: {
+    renderData() {
+      console.log(this.$route.params.postsId);
+    },
+  },
+  components: {
+    GoHome,
+  },
+  created() {
+    this.renderData();
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped lang="less"></style>

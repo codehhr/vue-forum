7f306bdf377681d550268e3572e493f87cfede8f
codehhr <1871973389@qq.com>
2021-08-03 22:26:25 +0800

    基本完成个人中心

--- src/views/UserCenter.vue
+++ src/views/UserCenter.vue
@@ -20,6 +20,15 @@
         <div class="username">
           {{ userInfo ? userInfo.userName : "~" }}
         </div>
+        <div class="remark">
+          {{
+            userInfo
+              ? userInfo.remark == 0
+                ? "这个人很懒，什么都没有写……"
+                : userInfo.remark
+              : "这个人很懒，什么都没有写……"
+          }}
+        </div>
 
         <!-- 头像上传 start -->
         <a-upload
@@ -38,19 +47,110 @@
         <!-- 头像上传 end -->
       </div>
     </div>
-
     <!-- 头像 end -->
+
+    <div class="user-info">
+      <div class="user-info-item">
+        <div class="username">
+          <span>昵称 : </span>{{ userInfo ? userInfo.userName : "userName" }}
+        </div>
+      </div>
+      <div class="user-info-item">
+        <div class="email">
+          <span>邮箱 : </span>{{ userInfo ? userInfo.email : "email" }}
+        </div>
+      </div>
+      <div class="user-info-item">
+        <div class="phonenumber">
+          <span>手机号 : </span
+          >{{ userInfo ? userInfo.phonenumber : "phonenumber" }}
+        </div>
+      </div>
+      <div class="user-info-item">
+        <div class="sex">
+          <span>性别 : </span>
+          {{ userInfo ? (userInfo.sex == 0 ? "男" : "女") : "sex" }}
+        </div>
+      </div>
+      <van-cell class="modify-userinfo-btn" @click="showModifyUserInfoPopup">
+        <a-button icon="setting">修改个人信息</a-button>
+      </van-cell>
+      <van-popup
+        class="modify-userinfo-popup"
+        position="right"
+        v-model="modifyUserInfoShow"
+      >
+        <go-back @close="closeModifyUserInfoPopup"></go-back>
+        <a-form
+          id="modifyinfo-form"
+          :form="form"
+          class="modifyinfo-form"
+          @submit="handleSubmit"
+        >
+          <a-form-item>
+            <a-input
+              v-decorator="['userName', {}]"
+              autocomplete
+              placeholder="请输入新的昵称"
+            />
+          </a-form-item>
+          <a-form-item>
+            <a-input
+              v-decorator="['email', {}]"
+              placeholder="请输入新的邮箱地址"
+              autocomplete
+            />
+          </a-form-item>
+          <a-form-item>
+            <a-input
+              v-decorator="['phonenumber', {}]"
+              autocomplete
+              placeholder="请输入新的手机号"
+              style="width: 100%"
+            >
+            </a-input>
+          </a-form-item>
+          <a-form-item>
+            <a-input
+              v-decorator="['sex', {}]"
+              autocomplete
+              placeholder="请输入性别"
+              style="width: 100%"
+            >
+            </a-input>
+          </a-form-item>
+          <a-form-item>
+            <a-input
+              v-decorator="['remark', {}]"
+              autocomplete
+              placeholder="请输入新的个性签名"
+              style="width: 100%"
+            >
+            </a-input>
+          </a-form-item>
+          <a-form-item>
+            <a-button type="primary" html-type="submit" class="submit-btn">
+              提交
+            </a-button>
+          </a-form-item>
+        </a-form>
+      </van-popup>
+    </div>
   </div>
 </template>
 
 <script>
+import { modifyUserInfo } from "../api/api";
 import { mapState } from "vuex";
 import GoHome from "../components/GoHome";
+import GoBack from "../components/GoBack";
 
 export default {
   name: "UserCenter",
   data() {
-    return {};
+    return {
+      modifyUserInfoShow: false,
+    };
   },
   methods: {
     // 更改头像
@@ -69,9 +169,43 @@ export default {
         this.$message.warning("请登录");
       }
     },
+    showModifyUserInfoPopup() {
+      this.modifyUserInfoShow = true;
+    },
+    // 修改个人信息
+    handleSubmit(e) {
+      e.preventDefault();
+      this.form.validateFieldsAndScroll((err, values) => {
+        if (!err) {
+          console.log(values);
+          modifyUserInfo(
+            values.userName,
+            values.email,
+            values.phonenumber,
+            values.sex,
+            values.remark
+          ).then((res) => {
+            if (res.code === 0) {
+              this.$message.success("修改成功 ~");
+              this.$store.dispatch("checkAlreadyLogin");
+              this.modifyUserInfoShow = false;
+            } else {
+              this.$message.success(res.msg);
+            }
+          });
+        }
+      });
+    },
+    closeModifyUserInfoPopup(data) {
+      this.modifyUserInfoShow = data;
+    },
+  },
+  beforeCreate() {
+    this.form = this.$form.createForm(this, { name: "normal_login" });
   },
   components: {
     GoHome,
+    GoBack,
   },
   computed: {
     ...mapState({
@@ -83,6 +217,10 @@ export default {
 </script>
 
 <style scoped lang="less">
+@user-info-item-span-font-size: 1.2rem;
+@user-info-item-font-size: 1rem;
+@main-color: #4d698e;
+
 .user {
   height: 100vh;
   .card {
@@ -112,15 +250,44 @@ export default {
         background-size: cover !important;
         overflow: hidden;
       }
-      .username {
-        margin: 10px 0;
+      .username,
+      .remark {
+        margin: 6px 0;
         font-size: 1.2rem;
-        color: #000000;
+        color: #000000aa;
       }
       .upload {
         .upload-btn {
           border: none;
         }
+      }
+    }
+  }
+  .user-info {
+    padding: 40px;
+    height: calc(100vw - 46px);
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    .user-info-item {
+      margin: 10px 0;
+      height: 36px;
+      color: #575a5a;
+      font-size: @user-info-item-font-size;
+      span {
+        font-size: @user-info-item-span-font-size;
+        color: @main-color;
+      }
+    }
+  }
+  .modify-userinfo-popup {
+    width: 100%;
+    height: 100%;
+    .modifyinfo-form {
+      padding: 40px;
+      .submit-btn {
+        width: 100%;
       }
     }
   }

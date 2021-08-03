7c9b45c41fe83bc2ca2582eec2e1b0d91b22aa8f
codehhr <1871973389@qq.com>
2021-08-04 00:56:28 +0800

    rename Post -> PostText || PostHtml

--- src/components/PostHtml.vue
+++ src/components/PostHtml.vue
@@ -9,7 +9,7 @@ import { mavonEditor } from "mavon-editor";
 import "mavon-editor/dist/css/index.css";
 
 export default {
-  name: "Post",
+  name: "PostText",
   data() {
     return {
       editShow: false,
---
+++ src/components/PostText.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "PostHtml",
+};
+</script>
+
+<style lang="less"></style>

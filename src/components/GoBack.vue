7268fa11b2056d33deb145a049c58a743e311d1f
codehhr <1871973389@qq.com>
2021-08-03 22:26:52 +0800

    add GoBack component for modify userInfo

---
+++ src/components/GoBack.vue
@@ -0,0 +1,31 @@
+<template>
+  <div @click="goBack" class="go-back">
+    <van-button class="go-back-btn" icon="arrow-left" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GoBack",
+  methods: {
+    goBack() {
+      this.$emit("close", false);
+    },
+  },
+};
+</script>
+
+<style scoped lang="less">
+.go-back {
+  padding: 0 20px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  height: 46px;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.4);
+  .go-back-btn {
+    padding: 0;
+  }
+}
+</style>

137008a4077c679daa9c1c7d85b050218808ec5d
codehhr <1871973389@qq.com>
2021-08-03 19:11:01 +0800

    create Announcement

---
+++ src/components/Announcement.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- 公告 -->
+  <div class="announcement">
+    <slot name="category"></slot>
+    <slot name="title"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Announcement",
+};
+</script>
+
+<style scoped lang="less">
+@announcement-bg-color: #e8ecf3;
+
+.announcement {
+  height: 100px;
+  background-color: @announcement-bg-color;
+}
+</style>

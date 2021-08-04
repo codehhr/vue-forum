3c820d5b6cb2f5093f512dd2590ad15cc89d00f3
codehhr <1871973389@qq.com>
2021-08-04 18:59:43 +0800

    添加公告插件,基本完成

--- src/components/Announcement.vue
+++ src/components/Announcement.vue
@@ -1,12 +1,7 @@
 <template>
   <!-- 公告 -->
   <div class="announcement">
-    <div class="catrgory-tag">
-      <a-tag :color="color">
-        {{ categoryName }}
-        程序员
-      </a-tag>
-    </div>
+    <slot name="category"></slot>
     <slot name="title"></slot>
   </div>
 </template>
@@ -22,64 +17,7 @@ export default {
       color: "#87d068",
     };
   },
-  created() {
-    switch (this.categoryId) {
-      case 1:
-        this.color = "#87d068";
-        break;
-      case 2:
-        this.color = "#2db7f5";
-        break;
-      case 3:
-        this.color = "#4b93d1";
-        break;
-      case 4:
-        this.color = "#00ccff";
-        break;
-      case 5:
-        this.color = "#bf5255";
-        break;
-      case 6:
-        this.color = "#4b8f83";
-        break;
-      case 7:
-        this.color = "#d68b4f";
-        break;
-      case 8:
-        this.color = "#a0e585";
-        break;
-      case 9:
-        this.color = "#fdc086";
-        break;
-      case 10:
-        this.color = "#9354ca";
-        break;
-      case 11:
-        this.color = "#e8ecf3";
-        break;
-      case 12:
-        this.color = "#a0e585";
-        break;
-      case 13:
-        this.color = "#d68b4f";
-        break;
-      case 14:
-        this.color = "#bf5255";
-        break;
-      case 15:
-        this.color = "#00ccff";
-        break;
-      case 16:
-        this.color = "#4b93d1";
-        break;
-      case 17:
-        this.color = "#2db7f5";
-        break;
-      default:
-        this.color = "#494f5c";
-        break;
-    }
-  },
+  created() {},
   computed: {
     ...mapState({
       categoryId: "categoryId",
@@ -90,18 +28,15 @@ export default {
 </script>
 
 <style scoped lang="less">
-@announcement-bg-color: #e8ecf3;
 @catrgory-tag-span-font-size: 1rem;
 
 .announcement {
   height: 100px;
-  background-color: @announcement-bg-color;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   .catrgory-tag {
-    margin: 10px 0;
     span {
       padding: 4px 8px;
       font-size: @catrgory-tag-span-font-size;
@@ -110,12 +45,4 @@ export default {
   }
 }
 </style>
-<style lang="less">
-.announcement {
-  > div {
-    h2 {
-      letter-spacing: 0.8rem;
-    }
-  }
-}
-</style>
+<style lang="less"></style>

427af75fc7fd84dc2b58fcea5fc5d8ba25aa2fb6
codehhr <1871973389@qq.com>
2021-08-04 19:00:40 +0800

    Adjust style

--- src/components/SigninEntrance.vue
+++ src/components/SigninEntrance.vue
@@ -31,6 +31,7 @@ export default {
 
 <style scoped lang="less">
 @main-color: #4d698e;
+@main-bg-color: #e8ecf3;
 
 .signin-entrance {
   margin-top: 10px;
@@ -41,16 +42,25 @@ export default {
   justify-content: space-between;
   .login,
   .register {
+    margin: 1px 0;
+    padding: 0 10px;
+    width: 100%;
     height: 36px;
+    &:active {
+      background-color: @main-bg-color;
+    }
   }
   a {
-    margin-top: 10px;
+    width: 100%;
+    display: flex;
+    justify-content: flex-start;
     .login-btn,
     .register-btn {
       padding: 0;
       font-size: 1.2rem;
-      height: 28px;
+      height: 36px;
       color: @main-color;
+      background-color: transparent;
     }
   }
 }

e3dda74adfe74b13987a09b5794c255a2bba93b4
codehhr <1871973389@qq.com>
2021-08-05 15:16:35 +0800

    _

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -14,42 +14,17 @@
 
       <!-- 发表新帖 -->
       <template v-slot:right>
-        <!-- <router-link to="/post">
-        </router-link> -->
-        <a-dropdown class="edit" :trigger="['click']">
-          <a-menu class="edit-menu" slot="overlay" @click="handleMenuClick">
-            <a-menu-item class="edit-menu-item" key="1"
-              ><a-icon type="edit" />文本</a-menu-item
-            >
-            <a-menu-item class="edit-menu-item" key="2">
-              <span v-text="'</>副文本'"></span>
-            </a-menu-item>
-          </a-menu>
-          <a-button style="width: 24px;height: 100%;border: none;">
-            <a-icon class="edit" type="form" />
-          </a-button>
-        </a-dropdown>
+        <!-- editor-btn -->
+        <a-button @click="showEditorPopup" class="edit-btn" icon="edit" />
         <!-- editor -->
         <van-popup class="editor-popup" position="right" v-model="editorShow">
-          <go-back @close="showEditor">
-            <template v-slot:publish>
-              <van-icon
-                @click="confirmPublish"
-                class="publish-icon"
-                name="share"
-              />
-              <van-dialog
-                v-model="showDialog"
-                title="确定发表吗"
-                show-cancel-button
-                class="dialog"
-              >
-                <img width="100%" src="https://img01.yzcdn.cn/vant/doge.png" />
-              </van-dialog>
-            </template>
-          </go-back>
-          <post-text class="post" v-show="key == 1"></post-text>
-          <post-html class="post" v-show="key == 2"></post-html>
+          <!-- 返回 -->
+          <go-back @close="closeEditor"></go-back>
+          <!-- post -->
+          <post
+            @closeEditorAfterPost="closeEditorAfterPost"
+            class="post"
+          ></post>
         </van-popup>
       </template>
     </page-header>
@@ -66,10 +41,9 @@ import PageHeader from "../components/PageHeader";
 import HeaderNav from "../components/HeaderNav";
 import SwitchTopics from "../components/SwitchTopics";
 import PostList from "../components/PostList";
-import { mapState } from "vuex";
-import PostText from "../components/PostText";
-import PostHtml from "../components/PostHtml";
+// import { mapState } from "vuex";
 import GoBack from "../components/GoBack";
+import Post from "../components/Post";
 
 export default {
   name: "Index",
@@ -77,72 +51,44 @@ export default {
     return {
       // 显示编辑器
       editorShow: false,
-      // 编辑选项
-      key: null,
-      showDialog: false,
     };
   },
   methods: {
-    // 点击编辑选项
-    handleMenuClick(values) {
-      if (values.key == 1) {
-        this.editorShow = true;
-        this.key = 1;
-      } else {
-        this.editorShow = true;
-        this.key = 2;
-      }
+    // 显示发表页
+    showEditorPopup() {
+      this.editorShow = true;
     },
-    // 关闭 editor
-    showEditor(data) {
+    closeEditor(data) {
       this.editorShow = data;
     },
     // 确认发表
     confirmPublish() {
       this.showDialog = true;
     },
+    // 发表后回关闭编辑页
+    closeEditorAfterPost(data) {
+      this.editorShow = data;
+    },
   },
   mounted() {
     this.container = this.$refs.container;
   },
-  created() {
-    if (this.hasNotificate) {
-      return;
-    } else {
-      this.$notification.open({
-        message: "提示",
-        description: "欢迎来到 亿个人的论坛",
-        style: {
-          width: "300px",
-          marginLeft: `calc(${100}% - 300px)`,
-        },
-      });
-      this.$store.commit("setNotificate", true);
-    }
-  },
-  computed: {
-    ...mapState({
-      hasNotificate: "hasNotificate",
-    }),
-  },
+  created() {},
+  computed: {},
   components: {
     PageHeader,
     HeaderNav,
     SwitchTopics,
     PostList,
-    PostText,
-    PostHtml,
+    Post,
     GoBack,
   },
 };
 </script>
 
 <style scoped lang="less">
-.edit-menu {
-  border-radius: 10px;
-  .edit-menu-item {
-    padding: 10px 40px;
-  }
+.edit-btn {
+  border: none;
 }
 .editor-popup {
   width: 100%;

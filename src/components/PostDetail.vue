e7c43b9b5d6228344d5a7a2eec774592320f540a
codehhr <1871973389@qq.com>
2021-08-06 23:45:45 +0800

    完成一级评论列表渲染

--- src/components/PostDetail.vue
+++ src/components/PostDetail.vue
@@ -1,35 +1,364 @@
 <template>
   <div class="postdetail">
-    <go-home></go-home>
-    <!-- 公告 -->
-    <announcement></announcement>
-    <div class="post-detail">
-      <div class="post-detail-header"></div>
-      <div class="post-detail-body"></div>
-      <div class="post-detail-comments"></div>
+    <go-back @close="closePostDetailPopup"></go-back>
+    <div class="post-detail-outer">
+      <div class="post-detail">
+        <div class="post-detail-header">
+          <!-- 标题 -->
+          <announcement>
+            <template v-slot:category>
+              <div>
+                <a-tag color="#f5f5f5">
+                  <h2
+                    :style="{ color: color }"
+                    class="category-name"
+                    v-text="renderPostDetailCategoryName()"
+                  ></h2>
+                </a-tag>
+              </div>
+            </template>
+            <template v-slot:title>
+              <div>
+                <h2
+                  class="post-title"
+                  v-html="postItemAndCategoryList.postItem.title"
+                ></h2>
+              </div>
+            </template>
+          </announcement>
+        </div>
+        <!-- detail -->
+        <div class="post-detail-body">
+          <!-- avatar -->
+          <div
+            class="avatar"
+            :style="
+              'background: url(' +
+                postItemAndCategoryList.postItem.avatar +
+                ') center center no-repeat'
+            "
+          ></div>
+          <!-- content -->
+          <div class="post-detail-content">
+            <img
+              class="cover"
+              :src="postItemAndCategoryList.postItem.coverImgUrl"
+              alt=""
+            />
+            <div
+              class="post-intro"
+              v-html="postItemAndCategoryList.postItem.intro"
+            ></div>
+            <div class="post-detail-content-footer">
+              <div class="create-time">
+                <a-icon type="file-markdown" /><span
+                  >发表于{{ postItemAndCategoryList.postItem.sendTime }}</span
+                >
+              </div>
+              <div
+                v-show="postItemAndCategoryList.postItem.updateTime"
+                class="update-time"
+              >
+                <a-icon type="history" /><span
+                  >更新于{{ postItemAndCategoryList.postItem.updateTime }}</span
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- comments start -->
+        <div class="post-detail-comments">
+          <div class="comment">
+            <!-- 当前用户评论 start -->
+            <a-comment>
+              <a-avatar
+                slot="avatar"
+                :src="
+                  userInfo
+                    ? userInfo.avatar
+                    : 'https://b.yzcdn.cn/vant/icon-demo-1126.png'
+                "
+                :alt="userInfo ? userInfo.userName : ''"
+              />
+              <div slot="content">
+                <a-form-item>
+                  <a-textarea
+                    :rows="3"
+                    :value="userCommentValue"
+                    placeholder="来发评论吧 ~ (支持副文本)"
+                    @change="handleChange"
+                  />
+                </a-form-item>
+                <a-form-item>
+                  <a-button
+                    html-type="submit"
+                    :loading="submitting"
+                    type="primary"
+                    @click="handleSubmit"
+                  >
+                    添加评论
+                  </a-button>
+                </a-form-item>
+              </div>
+            </a-comment>
+            <!-- 当前用户评论 end -->
+
+            <!-- 评论列表 start -->
+            <a-list
+              class="comment-list"
+              :header="`共 ${commentsList.length} 条回复`"
+              item-layout="horizontal"
+              :data-source="commentsList"
+            >
+              <a-list-item slot="renderItem" slot-scope="item">
+                <div class="reply-list">
+                  <a-comment :author="item.userName" :avatar="item.avatar">
+                    <p v-html="item.commentContent" slot="content"></p>
+                    <a-tooltip slot="datetime" :title="item.updatedTime">
+                      <span>{{ item.updatedTime }}</span>
+                    </a-tooltip>
+                    <template slot="actions">
+                      <span key="comment-basic-like">
+                        <a-tooltip title="Like">
+                          <a-icon
+                            type="like"
+                            :theme="
+                              `action${item.commentId}` === `liked`
+                                ? 'filled'
+                                : 'outlined'
+                            "
+                            @click="like(item.commentId)"
+                          />
+                        </a-tooltip>
+                        <span
+                          :ref="`like${item.commentId}`"
+                          style="padding-left: '8px';cursor: 'auto'"
+                        >
+                          0
+                        </span>
+                      </span>
+                      <a-divider type="vertical" />
+                      <span key="comment-basic-reply-to">回复</span>
+                    </template>
+                  </a-comment>
+                </div>
+              </a-list-item>
+            </a-list>
+            <!-- 评论列表 end -->
+          </div>
+        </div>
+        <!-- comments end -->
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import GoHome from "../components/GoHome";
-import Announcement from "../components/Announcement";
+import { mapState } from "vuex";
+import Announcement from "./Announcement";
+import { getCommentsList, postComment } from "../api/api";
+import GoBack from "./GoBack";
+import moment from "moment";
 
 export default {
   name: "PostDetail",
-  methods: {
-    renderData() {
-      console.log(this.$route.params.postsId);
+  data() {
+    return {
+      // 评论列表
+      commentsList: [],
+      submitting: false,
+      // 当前用户要提交的评论内容
+      userCommentValue: "",
+      // 赞数量
+      zan: 0,
+      // 点赞动作
+      action: null,
+      moment,
+      // 每一项
+      postItem: {},
+      // 分类数组
+      postCategoryList: [],
+    };
+  },
+  props: {
+    // 从 postList 传过来的 item 和 分类的数组
+    postItemAndCategoryList: {
+      type: Object,
     },
   },
-  components: {
-    GoHome,
-    Announcement,
+  methods: {
+    // 点赞
+    like(commentId) {
+      this[`action${commentId}`] = `liked`;
+      this.$refs[`like${commentId}`].innerHTML = 1;
+    },
+    // 渲染数据
+    // 渲染详情页标签名
+    renderPostDetailCategoryName() {
+      // 渲染评论
+      this.renderCommentsList(this.postItemAndCategoryList.postItem.postsId);
+      // 分类标签名
+      let tagName = "";
+      this.postItemAndCategoryList.postCategoryList.forEach((item) => {
+        if (
+          item.categoryId == this.postItemAndCategoryList.postItem.categoryId
+        ) {
+          tagName = item.name;
+        }
+      });
+      return tagName;
+    },
+    // 关闭详情页
+    closePostDetailPopup() {
+      this.$store.commit("setPostDetailShow", false);
+    },
+    // 同步 data 于输入框里的值
+    handleChange(e) {
+      this.userCommentValue = e.target.value;
+    },
+    // 提交评论 (非回复)
+    handleSubmit() {
+      if (!this.userCommentValue) {
+        return;
+      } else {
+        this.submitting = true;
+        setTimeout(() => {
+          this.submitting = false;
+
+          postComment({
+            postsId: this.postItemAndCategoryList.postItem.postsId,
+            commentContent: this.userCommentValue,
+          }).then((res) => {
+            if (res.code === 0) {
+              this.$message.success("提交成功");
+              this.renderCommentsList(
+                this.postItemAndCategoryList.postItem.postsId
+              );
+            }
+          });
+          this.userCommentValue = "";
+        }, 500);
+      }
+    },
+
+    // 渲染评论列表
+    renderCommentsList(postsId) {
+      getCommentsList({ postsId, pageNum: 1, pageSize: 10 }).then((res) => {
+        this.commentsList = res.rows;
+      });
+    },
   },
-  created() {
-    this.renderData();
+  computed: {
+    ...mapState({
+      color: "color",
+      userInfo: "userInfo",
+    }),
+  },
+  components: {
+    Announcement,
+    GoBack,
   },
 };
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+@title-color: #ffffff;
+@header-bg-color: #7cb6e9;
+@title-font-size: 1.4rem;
+@category-name-font-size: 0.9rem;
+@post-intro-font-size: 1.2rem;
+@post-detail-content-footer-font-size: 0.6rem;
+@post-detail-content-footer-color: #aaaaaa;
+
+.ant-tag.ant-tag-has-color {
+  margin: 0;
+  padding: 0;
+  border: none;
+  border-radius: 6px;
+  .category-name {
+    margin: 0;
+    padding: 1px 6px;
+    font-size: @category-name-font-size;
+  }
+}
+.post-detail-outer {
+  height: calc(100vh - 46px);
+  overflow-y: auto;
+  .post-detail {
+    // 头部
+    .post-detail-header {
+      background-color: @header-bg-color;
+      .post-title {
+        margin: 10px auto 0;
+        font-size: @title-font-size;
+        color: @title-color;
+      }
+    }
+    // 帖子内容
+    .post-detail-body {
+      padding: 20px 10px;
+      .avatar {
+        width: 32px;
+        height: 32px;
+        border-radius: 50%;
+        background-size: cover !important;
+      }
+      .post-detail-content {
+        padding: 0 40px;
+
+        .cover {
+          width: 100%;
+          border-radius: 8px;
+        }
+        .post-intro {
+          margin: 20px 0;
+          text-align: left;
+          font-size: @post-intro-font-size;
+          word-break: break-all;
+        }
+        .post-detail-content-footer {
+          text-align: left;
+          font-size: @post-detail-content-footer-font-size;
+          color: @post-detail-content-footer-color;
+          i {
+            margin-right: 4px;
+            color: @post-detail-content-footer-color !important;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
+<style lang="less">
+// 评论
+.post-detail-comments {
+  padding: 10px 40px 10px 10px;
+  .ant-comment-inner {
+    padding: 0;
+    .ant-comment-content-detail {
+      padding: 5px;
+    }
+  }
+  .like {
+    width: 100%;
+    text-align: right;
+    .reply {
+      border: none;
+    }
+  }
+}
+.comment-list {
+  .reply-list {
+    width: 100%;
+    .ant-comment-content {
+      .ant-comment-actions {
+        text-align: right;
+      }
+    }
+  }
+}
+.ant-comment-actions > li > span {
+  padding: 0 !important;
+}
+</style>

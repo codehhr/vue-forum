34bcb509fe8f540597a6e02bf6328b15287c472e
codehhr <1871973389@qq.com>
2021-08-07 19:22:36 +0800

    关于本站

---
+++ src/components/Site.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="site">
+    <!-- github -->
+    <a href="https://github.com/codehhr/vue-forum">
+      <a-icon class="github-icon" type="github" theme="filled" />
+    </a>
+    <!-- gitee -->
+    <a href="https://gitee.com/codehhr/vue-forum">
+      <a-icon type="gitee" theme="filled" />
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Site",
+};
+</script>
+
+<style lang="less">
+@icon-font-size: 2rem;
+
+.site {
+  padding: 20px;
+  .github-icon {
+    font-size: @icon-font-size;
+    color: #000000 !important;
+  }
+}
+</style>
